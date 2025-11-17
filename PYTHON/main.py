c60eab547e7f941227c03246ca38658be1c55c21
KSJ <yaja7698@naver.com>
2025-11-18 08:43:10 +0900

    [update]

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,5 +1,13 @@
-from modbus.client import ModbusTCPClient
-from modbus.server import ModbusTCPServer
-from robot.robot import dobot
 from uart.uart import uart
-from vision.vision import vision
+import time
+
+a = uart('COM7', 115200)
+
+while True:
+    receive_data = a.receive()
+    print(f"데이터 수신을 대기합니다.")
+    if (receive_data):
+        print(f"데이터를 수신받았습니다.")
+        print(f"{receive_data}")
+        break
+    
